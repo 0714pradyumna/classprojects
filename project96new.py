c6d34e3573d59b15a0532a2f8913b45e4e03f556
0714pradyumna <96380416+0714pradyumna@users.noreply.github.com>
2022-01-12 10:21:41 +0530

    Update project96new.py

--- project96new.py
+++ project96new.py
@@ -1,7 +1,50 @@
+import numpy as np
+import pandas as pd
+
+@st.cache()
+def load_data():
+	# Load the Adult Income dataset into DataFrame.
+
+	df = pd.read_csv('https://student-datasets-bucket.s3.ap-south-1.amazonaws.com/whitehat-ds-datasets/adult.csv', header=None)
+	df.head()
+
+	# Rename the column names in the DataFrame using the list given above. 
+
+	# Create the list
+	column_name =['age', 'workclass', 'fnlwgt', 'education', 'education-years', 'marital-status', 'occupation', 
+               'relationship', 'race','gender','capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
+
+	# Rename the columns using 'rename()'
+	for i in range(df.shape[1]):
+	  df.rename(columns={i:column_name[i]},inplace=True)
+
+	# Print the first five rows of the DataFrame
+	df.head()
+
+	# Replace the invalid values ' ?' with 'np.nan'.
+
+	df['native-country'] = df['native-country'].replace(' ?',np.nan)
+	df['workclass'] = df['workclass'].replace(' ?',np.nan)
+	df['occupation'] = df['occupation'].replace(' ?',np.nan)
+
+	# Delete the rows with invalid values and the column not required 
+
+	# Delete the rows with the 'dropna()' function
+	df.dropna(inplace=True)
+
+	# Delete the column with the 'drop()' function
+	df.drop(columns='fnlwgt',axis=1,inplace=True)
+
+	return df
+
+census_df = load_data()
+
+
+
+
 # Configure the main page by setting its title and icon that will be displayed in a browser tab.
 # Import the streamlit Python module.
 import streamlit as st
-census_df = load_data()
 # Configure your home page.
 st.set_page_config(page_title = 'Census Visualisation Web App',
                    page_icon = '*',
